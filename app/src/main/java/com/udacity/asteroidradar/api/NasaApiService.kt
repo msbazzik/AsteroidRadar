4e6909bfa271d8adf4c74a1744576eacdd248b40
Valery Basova <basovavalery@gmail.com>
2022-07-11 17:44:28 -0400

    add retrofit

---
+++ app/src/main/java/com/udacity/asteroidradar/api/NasaApiService.kt
@@ -0,0 +1,28 @@
+package com.udacity.asteroidradar.api
+
+import com.udacity.asteroidradar.Constants.BASE_URL
+import retrofit2.Call
+import retrofit2.Retrofit.Builder
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private val retrofit = Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NasaApiService {
+    @GET("neo/rest/v1/feed")
+    fun getAsteroids(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("api_key") apiKey: String = "DEMO_KEY"
+    ): Call<String>
+}
+
+object NasaApi {
+    val retrofitService: NasaApiService by lazy {
+        retrofit.create(NasaApiService::class.java)
+    }
+}
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,35 @@
 package com.udacity.asteroidradar.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.udacity.asteroidradar.api.NasaApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainViewModel : ViewModel() {
+    private val _response = MutableLiveData<String>()
+
+    val response: LiveData<String>
+        get() = _response
+
+    init {
+        getAsteroidList()
+    }
+
+    private fun getAsteroidList() {
+        NasaApi.retrofitService.getAsteroids("2022-07-11", "2022-07-12")
+            .enqueue(object : Callback<String> {
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    _response.value = "Failure: " + t.message
+                }
+
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    val resp = response.body().toString()
+                    _response.value = resp
+                }
+            })
+        _response.value = "Set the Nasa API Response here!"
+    }
 }
