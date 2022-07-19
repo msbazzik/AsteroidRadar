c8d7704f97bcd5bd66c7e6c74fd0e05b9a2d8143
Valery Basova <basovavalery@gmail.com>
2022-07-19 17:23:19 -0400

    set constraints to WorkManager

--- app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -1,9 +1,8 @@
 package com.udacity.asteroidradar
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.udacity.asteroidradar.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -23,7 +22,18 @@ class AsteroidRadarApplication : Application() {
     }
 
     private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
             .build()
         WorkManager.getInstance().enqueueUniquePeriodicWork(
             RefreshDataWorker.WORK_NAME,
--- app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
+++ app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
@@ -1,17 +1,15 @@
 package com.udacity.asteroidradar.work
 
-import android.app.Application
 import android.content.Context
 import androidx.work.CoroutineWorker
-import androidx.work.ListenableWorker
 import androidx.work.WorkerParameters
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.repository.AsteroidsRepository
 import retrofit2.HttpException
 
-class RefreshDataWorker (appContext: Context, params: WorkerParameters) :
-CoroutineWorker(appContext, params) {
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
 
     companion object {
         const val WORK_NAME = "RefreshDataWorker"
