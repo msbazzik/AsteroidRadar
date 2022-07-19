420ee6d7bcefaa2132f5e133166ab0d955a9f70d
Valery Basova <basovavalery@gmail.com>
2022-07-19 14:57:54 -0400

    add LocalDate start and end days

--- app/src/main/java/com/udacity/asteroidradar/repository/AsteroidsRepository.kt
+++ app/src/main/java/com/udacity/asteroidradar/repository/AsteroidsRepository.kt
@@ -2,25 +2,41 @@ package com.udacity.asteroidradar.repository
 
 import androidx.lifecycle.LiveData
 import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.api.NasaApi
 import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
 import com.udacity.asteroidradar.database.AsteroidDatabase
 import com.udacity.asteroidradar.database.NasaDatabaseFilter
 import org.json.JSONObject
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class AsteroidsRepository(private val database: AsteroidDatabase) {
+    private val startDay: String
+        get() {
+            val current = LocalDate.now()
+            val formatter = DateTimeFormatter.ofPattern(Constants.API_QUERY_DATE_FORMAT)
+            return current.format(formatter)
+        }
+
+    private val endDay: String
+        get() {
+            val current = LocalDate.now().plusDays(Constants.DEFAULT_END_DATE_DAYS.toLong())
+            val formatter = DateTimeFormatter.ofPattern(Constants.API_QUERY_DATE_FORMAT)
+            return current.format(formatter)
+        }
 
     fun getAsteroidsList(filter: NasaDatabaseFilter): LiveData<List<Asteroid>> {
         return when (filter) {
-            NasaDatabaseFilter.WEEK_ASTEROIDS -> database.databaseDao.getAsteroids("2022-07-15", "2022-07-22")
-            NasaDatabaseFilter.TODAY_ASTEROIDS -> database.databaseDao.getAsteroids("2022-07-15")
+            NasaDatabaseFilter.WEEK_ASTEROIDS -> database.databaseDao.getAsteroids(startDay, endDay)
+            NasaDatabaseFilter.TODAY_ASTEROIDS -> database.databaseDao.getAsteroids(startDay)
             else -> database.databaseDao.getAsteroids()
         }
     }
 
     suspend fun fetchAsteroids(apiKey: String) {
         val response = NasaApi.retrofitScalarsService.getAsteroids(
-            "2022-07-15", "2022-07-22", apiKey
+            startDay, endDay, apiKey
         )
         val parsedAsteroid =
             parseAsteroidsJsonResult(JSONObject(response))
